a8062abb2d19855d1e3bbecd7a9a688842b87779
Rafa AC <Rafa@cev.es>
2026-04-14 19:40:31 +0200

    add : Creacion Modelos 4 y repository

--- src/main/java/com/demo/backGestor/modelos/RegistroMaterial.java
+++ src/main/java/com/demo/backGestor/modelos/RegistroMaterial.java
@@ -1,4 +1,80 @@
 package com.demo.backGestor.modelos;
 
+import jakarta.persistence.*;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="REGISTRO_MATERIALES")
 public class RegistroMaterial {
+/*
+CREATE TABLE REGISTRO_MATERIALES(
+    ID_MATERIAL INT ,
+    ID_TRABAJO INT ,
+    CANTIDAD INT(10),
+    FECHA DATE ,
+    CONSTRAINT REGISTRO_MATERIAL  FOREIGN KEY (ID_MATERIAL) REFERENCES MATERIALES(ID_MATERIAL) ,
+    CONSTRAINT REGISTRO_TRABAJOM  FOREIGN KEY (ID_TRABAJO) REFERENCES TRABAJOS(ID_TRABAJO) ,
+    CONSTRAINT PK_REGISTRO_MATERIAL PRIMARY KEY (ID_MATERIAL , ID_TRABAJO)
+);
+ */
+    @EmbeddedId
+    private RegistroMaterialId id ;
+
+    @ManyToOne
+    @MapsId("idTrabajo")
+    @JoinColumn(name = "ID_TRABAJO")
+    private Trabajo trabajo;
+
+    @ManyToOne
+    @MapsId("idMaterial")
+    @JoinColumn(name = "ID_MATERIAL")
+    private Material material ;
+
+    @Column(name = "CANTIDAD" , length = 10)
+    private int cantidad;
+
+    @Column(name = "FECHA")
+    private Date fecha ;
+
+    public RegistroMaterialId getId() {
+        return id;
+    }
+
+    public void setId(RegistroMaterialId id) {
+        this.id = id;
+    }
+
+    public Trabajo getTrabajo() {
+        return trabajo;
+    }
+
+    public void setTrabajo(Trabajo trabajo) {
+        this.trabajo = trabajo;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public LocalDate getFecha() {
+        return fecha.toLocalDate();
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
--- src/main/java/com/demo/backGestor/modelos/RegistroMaterialId.java
+++ src/main/java/com/demo/backGestor/modelos/RegistroMaterialId.java
@@ -1,4 +1,49 @@
 package com.demo.backGestor.modelos;
 
-public class RegistroMaterialId {
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class RegistroMaterialId implements Serializable {
+    @Column(name = "ID_MATERIAL")
+    private Integer idMateril ;
+
+    @Column(name = "ID_TRABAJO")
+    private Integer idTrabajo ;
+
+    public RegistroMaterialId(Integer idMateril , Integer idTrabajo) {
+        this.idMateril = idMateril;
+        this.idTrabajo = idTrabajo;
+    }
+
+    public Integer getIdMateril() {
+        return idMateril;
+    }
+
+    public void setIdMateril(Integer idMateril) {
+        this.idMateril = idMateril;
+    }
+
+    public Integer getIdTrabajo() {
+        return idTrabajo;
+    }
+
+    public void setIdTrabajo(Integer idTrabajo) {
+        this.idTrabajo = idTrabajo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RegistroMaterialId that = (RegistroMaterialId) o;
+        return Objects.equals(idMateril, that.idMateril) && Objects.equals(idTrabajo, that.idTrabajo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idMateril, idTrabajo);
+    }
 }
